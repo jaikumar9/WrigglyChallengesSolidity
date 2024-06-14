6111cf29508a9da3c8d8a1502277c0abf2ef2821
jaikumar9 <Jaiagdayemawer556@gmail.com>
2024-06-14 18:31:16 +0530

    Add soon

---
+++ Hard/BytecodeOne.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BytecodeGetter {
+    function getBytecode() public view returns (bytes memory) {
+        bytes memory bytecode;
+        assembly {
+            // TODO
+        }
+        return bytecode;
+    }
+}
